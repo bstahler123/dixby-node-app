2bf73e30a045d7b93d91594ddeefcf02d776e1d8
brad stahler <bradstahler@brads-MacBook-Air.local>
2017-04-06 09:57:20 -0400

    1

--- app.js
+++ app.js
@@ -1,46 +1,83 @@
-// new app
-var fetchTweets = require('./Twitter');
-var command = process.argv[2];
-var SpotifyWebApi = require("spotify-web-api-node");
+var keys = require('./key.js');
+var Twitter = require('Twitter');
+var request = require('request');
+var SpotifyWebApi = require('spotify-web-api-node');
+var client = new Twitter(keys.twitterKeys);
+// twitter
+function fetchTweets(tweets) {
+    var params = { screen_name: 'brad_ucf', count: 20 };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                tweets[i];
+                console.log(tweets[i].text);
+            }
 
+        }
+        console.log(error)
+    });
+}
+
+// omdb 
+function fetchMovie() {
+
+    request(queryUrl, function(error, response, body) {
+
+
+        if (!error && response.statusCode === 200) {
+
+
+            var body = JSON.parse(body)
+            console.log(body.Title);
+            console.log(body.Year);
+            console.log(body.Rated);
+            console.log(body.Country);
+            console.log(body.Language);
+            console.log(body.Plot);
+            console.log(body.Actors);
+            console.log(body.Ratings[1]);
+
+        }
+    });
+}
+//Spotify
+function fetchSongs() {
+
+    var spotifyApi = new SpotifyWebApi({
+        clientId: keys.spotifyKeys.clientId,
+        clientSecret: keys.spotifyKeys.clientSecret,
+        redirectUri: keys.spotifyKeys.redirectUri,
+    });
 
 
     spotifyApi.searchTracks(trackName)
-        .then(function (data) {
+        .then(function(data) {
             console.log(data.body.tracks.items[0].artists[0].name);
             console.log(data.body.tracks.items[0].name);
             console.log(data.body.tracks.items[0].preview_url);
             console.log(data.body.tracks.items[0].album.name);
 
-            }, function (err) {
+        }, function(err) {
             console.error(err);
         });
 
 }
-
-
-
-var userinput = process.argv[2];
-
-var movieName = process.argv[3];
 var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "=&plot=short&r=json";
+var userinput = process.argv[2];
+var movieName = process.argv[3];
 var trackName = process.argv[3];
 
+// calling functions based on input
+switch (userinput) {
+    case "my-tweets":
+        fetchTweets();
+        break;
 
+    case "movie-this":
+        fetchMovie(queryUrl);
+        break;
 
-
-
-switch (command){
-	case "my-tweets":
-	fetchTweets();
-	break;
-	case "spotify-this-song"
-	pickSong(trackName);
-	break;
-	
+    case "spotify-this-song":
+        fetchSongs(trackName);
+        break;
 }
-
- 
-
-console.log(command);
-
--- key.js
+++ key.js
@@ -1,6 +1,14 @@
+
+
 exports.twitterKeys = {
   consumer_key: 'p8zk0r0XPEjZsjz6vBkYjDuFX',
   consumer_secret: 'ryXYELXXtitGoovJr3SPi7tdWLE1D4rYyMPjw7LlI0mrvapFqr',
   access_token_key: '848989430929121283-1aOzCdhltm3XEZIHdAUGCkWmtISEfBx',
   access_token_secret: 'v0rpsPFc0ATb5Eo8NvKEzhiRfVHy9GrDIJBp1DoEkxWvv'
 }
+exports.spotifyKeys = {
+	clientId:"bb6dd62ef67a41ce8372f420cf2ff1dc",
+	clientSecret: "5110660181594101aeca2c5eee62e155",
+	redirectUri: "https://appname.herokuapp.com/callback"
+
+};
